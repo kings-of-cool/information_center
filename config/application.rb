0c2a316a2b879d0cfbc01fc15df818710b1501fc
Yohei Goto <yohei_goto@diveintocode.jp>
2020-04-06 23:22:44 +0900

    :banana: アプリ名をKottiKottiに変更

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@ require 'rails/all'
 
 Bundler.require(*Rails.groups)
 
-module InformationCenter
+module KottiKotti
   class Application < Rails::Application
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
