c491e5e986c6812828a5dbd59b829c8b1cfd0ce4
Yohei Goto <yohei_goto@diveintocode.jp>
2020-04-07 21:23:49 +0900

    :peach: InformationCenterモデルのSpec

--- app/models/information_center.rb
+++ app/models/information_center.rb
@@ -1,2 +1,11 @@
 class InformationCenter < ApplicationRecord
+  before_create :confirm_singularity
+  validates :name, presence: true
+  validates :tel, presence: true, format: { with: /\A\d{10}$|^\d{11}\z/ }
+
+  private
+
+  def confirm_singularity
+    raise Exception.new(I18n.t("errors.messages.exist_already")) if InformationCenter.count > 0
+  end
 end
--- spec/models/information_center_spec.rb
+++ spec/models/information_center_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe InformationCenter, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "InformstionCenterは唯一であること" do
+    FactoryBot.create(:information_center)
+    expect{ FactoryBot.create(:information_center) }.to raise_error(I18n.t("errors.messages.exist_already"))
+  end
+
+  describe "validation" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:tel) }
+
+    describe "tel" do
+      using RSpec::Parameterized::TableSyntax
+      let(:information_center) { FactoryBot.build(:information_center, tel: tel) }
+      let(:tel) { '' }
+      subject { information_center.valid? }
+      context "ハイフンなし10桁or11桁のみを許可すること" do
+        where(:tel, :valid?) do
+          '1' | false
+          '12' | false
+          '123' | false
+          '1234' | false
+          '12345' | false
+          '123456' | false
+          '1234567' | false
+          '12345678' | false
+          '123456789' | false
+          '1234567891' | true
+          '090-9999-9999' | false
+        end
+        with_them do
+          it { is_expected.to eq valid? }
+        end
+      end
+    end
+  end
 end
