59189896608846b7abc877e17a33c461f3cc567a
Yohei Goto <yohei_goto@diveintocode.jp>
2020-04-08 21:58:29 +0900

    :peach: InformationCenter新規作成フロー

--- app/controllers/information_centers_controller.rb
+++ app/controllers/information_centers_controller.rb
@@ -6,11 +6,24 @@ class InformationCentersController < ApplicationController
   end
 
   def new
+    @information_center = InformationCenter.new
   end
 
   def create
+    @information_center = InformationCenter.new(information_center_params)
+    if @information_center.save
+      redirect_to information_centers_url
+    else
+      render 'new'
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def information_center_params
+    params.require(:information_center).permit(:name, :tel)
   end
 end
