d9d0a57dfa5ef6c723ccf2765662e9cdb59057dd
Yohei Goto <yohei_goto@diveintocode.jp>
2020-04-08 21:34:08 +0900

    :peach: InformaitonCenterトップページ雛形

--- app/controllers/information_centers_controller.rb
+++ app/controllers/information_centers_controller.rb
@@ -2,6 +2,7 @@ class InformationCentersController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @information_center = InformationCenter.find_or_initialize_by(id: 1)
   end
 
   def new
